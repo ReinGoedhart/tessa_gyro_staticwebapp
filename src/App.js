7d9196db5b07a90f37256b8e838cdb28df2b8e47
ReinGoedhart <RWgoedhart@gmail.com>
2024-04-28 17:00:28 +0200

    adding to develop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Rein Gaat lekker!
+          Edit <code>src/App.js</code> Rein Gaat lekker!, nu in develop
         </p>
         <a
           className="App-link"
